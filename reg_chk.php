136d512ce03d4c7ced9de9ed46721906a70d923d
user66924 <32358671+user66924@users.noreply.github.com>
2018-01-21 11:36:56 +0500

    Добавлена БД
    При регистрации записываем данные в БД

--- reg_chk.php
+++ reg_chk.php
@@ -1,71 +1,156 @@
 <?php
+$db_host = 'localhost';
+$db_port = '3306';
+$db_base = 'webtech';
+$db_usr = 'root';
+$db_pwd = '';
 
-    $userLogin = '';
-    $userName = '';
-    $userPasw = '';
-    $userPaswC = '';
-    $userMail = '';    
-    /*if(eregi("^$SERVER_ROOT", $_SERVER['HTTP_REFERER'])){*/
-    if(isset($_POST['userLogin'])) $userLogin = $_POST['userLogin'];
-    if(isset($_POST['userName'])) $userName = $_POST['userName'];
-    if(isset($_POST['userPasw'])) $userPasw = $_POST['userPasw'];
-    if(isset($_POST['userPaswС'])) $userPaswC = $_POST['userPaswС'];
-    if(isset($_POST['userMail'])) $userMail = $_POST['userMail'];
-    
-    /*добавить функции обезвреживания sql js html*/
-    $err = checkLogin($userLogin) .
-            checkName($userName). 
-            checkPasw($userPasw). 
-            checkPaswEq($userPasw, $userPaswC). 
-            checkEmail($userMail) ;
-    
-    function checkLogin($log) 
-    {
-        $val = trim($log);
+$db_conn = new mysqli($db_host, $db_usr, $db_pwd, $db_base, $db_port);
+if ($db_conn->connect_error) {
+    die('db Not connected /n' . $db_conn->connect_error);
+}
 
-        if ($val == '') {  return 'Не введен логин.'; } 
-        elseif (strlen($val) < 5) { return 'В логине должно быть не менее 5 символов.'; } 
-        elseif (preg_match('/[^a-zA-Z0-9_-]/', $val) ) { return 'В логине разрешены только a-z, A-Z, 0-9, - и _.';}
-        elseif (!checkLoginFree($val)) { return 'Логин занят.'; } 
-        else {return '';}
+$userLogin = '';
+$userName = '';
+$userPasw = '';
+$userPaswC = '';
+$userMail = '';
+
+if (!(isset($_POST['regFrm']) && $_POST['regFrm'] = 'y')) {    
+    header('Location:register_frm.html');
+    exit();
+}
+
+
+if (isset($_POST['userLogin']))
+    $userLogin = mysql_entities_fix_string($_POST['userLogin']);
+if (isset($_POST['userName']))
+    $userName = mysql_entities_fix_string($_POST['userName']);
+if (isset($_POST['userPasw']))
+    $userPasw = mysql_entities_fix_string($_POST['userPasw']);
+if (isset($_POST['userPaswС']))
+    $userPaswC = mysql_entities_fix_string($_POST['userPaswС']);
+if (isset($_POST['userMail']))
+    $userMail = mysql_entities_fix_string($_POST['userMail']);
+
+
+$err = checkLogin($userLogin) .
+        checkName($userName) .
+        checkPasw($userPasw) .
+        checkPaswEq($userPasw, $userPaswC) .
+        checkEmail($userMail);
+
+
+
+
+$userNotUniq = 0;
+$userInserted = 0;
+if ($err == '') {
+    $query_chk = "SELECT * FROM t_users WHERE log = '$userLogin';";
+    $result_chk = $db_conn->query($query_chk);
+    if (!$result_chk)
+        die($db_conn->error);
+    $userNotUniq = $result_chk->num_rows;
+
+    if /* ($rows_chk) */ ($userNotUniq) {
+        //echo 'Такой логин уже есть';            
+    } else {
+        $query = "INSERT INTO t_users(log,pass,email,name) VALUES('$userLogin', '$userPasw', '$userMail', '$userName');";
+
+        $result = $db_conn->query($query);
+        if (!$result)
+            die($db_conn->error);
+        $rows = $result->num_rows;
+
+        $userInserted = $db_conn->insert_id;
+        //echo $rows;
+        $result->close;
     }
 
-    function checkLoginFree($log) 
-    {
-        if(trim($log)) { return TRUE; }   /* здесь будем проверять уникальность в БД */
-        else {return FALSE;}
-    }
-    
-    function checkName($nam) 
-    {
-        return (trim($nam) == '') ? 'Не введено имя.' : '';                   
-    }
+    $db_conn->close;
+}
 
-    function checkPasw($pas)
-    {
-        $val = trim($pas);
-        if ($val == ''){ return 'Не введен пароль.';}
-        elseif (strlen($val) < 6) { return 'В пароле должно быть не менее 6 символов.';}
-        elseif (!preg_match('/[a-z]/', val)  || !preg_match('/[A-Z]/', $val) || !preg_match('/[0-9]/', $val))
-            { return 'Пароль требует 1 символа из каждого набора a-z, A-Z и 0-9.';}
-        else {return '';}
-    }
-    
-    function checkPaswEq($pas1, $pas2)
-    {        
-        if ($pas1 != $pas2){ return 'Пароли не совпадают.';}
-        else {return '';}
-    }    
+function checkLogin($log) {
+    $val = trim($log);
 
-    function checkEmail($email) 
-    {        
-        $val = trim($email);
-        if ($val == '') { return 'Не введен адрес электронной почты.';}
-        elseif (!((strpos($val, '.') > 0) && (strpos($val, '@') > 0)) || preg_match ('/[^a-zA-Z0-9.@_-]/', $val) )
-            {return 'Электронный адрес имеет неверный формат.';}
-        else {return '';}
-    }    
-    
+    if ($val == '') {
+        return 'Не введен логин.';
+    } elseif (strlen($val) < 5) {
+        return 'В логине должно быть не менее 5 символов.';
+    } elseif (preg_match('/[^a-zA-Z0-9_-]/', $val)) {
+        return 'В логине разрешены только a-z, A-Z, 0-9, - и _.';
+    } elseif (!checkLoginFree($val)) {
+        return 'Логин занят.';
+    } else {
+        return '';
+    }
+}
+
+function checkLoginFree($log) {
+    if (trim($log)) {
+        return TRUE;
+    } /* здесь будем проверять уникальность в БД */ else {
+        return FALSE;
+    }
+}
+
+function checkName($nam) {
+    return (trim($nam) == '') ? 'Не введено имя.' : '';
+}
+
+function checkPasw($pas) {
+    $val = trim($pas);
+    if ($val == '') {
+        return 'Не введен пароль.';
+    } elseif (strlen($val) < 6) {
+        return 'В пароле должно быть не менее 6 символов.';
+    } elseif (!preg_match('/[a-z]/', val) || !preg_match('/[A-Z]/', $val) || !preg_match('/[0-9]/', $val)) {
+        return 'Пароль требует 1 символа из каждого набора a-z, A-Z и 0-9.';
+    } else {
+        return '';
+    }
+}
+
+//db
+function checkPaswEq($pas1, $pas2) {
+    if ($pas1 != $pas2) {
+        return 'Пароли не совпадают.';
+    } else {
+        return '';
+    }
+}
+
+function checkEmail($email) {
+    $val = trim($email);
+    if ($val == '') {
+        return 'Не введен адрес электронной почты.';
+    } elseif (!((strpos($val, '.') > 0) && (strpos($val, '@') > 0)) || preg_match('/[^a-zA-Z0-9.@_-]/', $val)) {
+        return 'Электронный адрес имеет неверный формат.';
+    } else {
+        return '';
+    }
+}
+
+function dbQuery($query) {
+    global $db_conn;
+    $result = $db_conn->query($query);
+    if (!$result) {
+        die($db_conn->connect_error);
+    }
+    return $result;
+}
+
+function mysql_entities_fix_string(/* $connection, */ $string) {
+    return htmlentities(mysql_fix_string(/* $db_conn, */ $string));
+}
+
+function mysql_fix_string(/* $connection, */ $string) {
+    if (get_magic_quotes_gpc()) {
+        $string = stripslashes($string);
+    }
+    global $db_conn;
+    return $db_conn->real_escape_string($string);
+}
 ?>
 
 <html>
@@ -75,32 +160,41 @@
     </head>
     <body>
         <?php
+        global $userLogin;
+        global $userName;
+        global $userPasw;
+        global $userPaswC;
+        global $userMail;
+        global $err;
+        if ($err != '') {
+            echo '<p style="background: red; color: white;">';
+            echo 'Проверка выявила ошибки:<br/>' . $err;
+            echo 'Необходимо откорректировать ввод!<br/>';
+        } else {
+            echo '<p style="background: green; color: white;">';
+            echo 'Проверено. Ввод коректный. Регистрируем польователя:<br/>';
+        }
 
-            global $userLogin;
-            global $userName;
-            global $userPasw;
-            global $userPaswC;
-            global $userMail; 
-            global $err;
-            if($err != '') 
-            {   
-                echo '<p style="background: red; color: white;">';
-                echo 'Проверка выявила ошибки:<br/>'.$err;
-                echo 'Необходимо откорректировать ввод!<br/>';
+        echo 'login = ' . $userLogin . '<br/>' .
+        'name = ' . $userName . '<br/>' .
+        'pass = ' . $userPasw . '<br/>' .
+        'pass2 = ' . $userPaswC . '<br/>' .
+        'email = ' . $userMail;
+        echo '</p>';
 
-            }
-            else 
-             { 
-                echo '<p style="background: green; color: white;">';
-                echo 'Проверено. Ввод коректный. Регистрируем польователя:<br/>';             
-            }
-            
-            echo  'login = '.$userLogin. '<br/>'.
-                    'name = '.$userName. '<br/>'.
-                    'pass = '.$userPasw. '<br/>'.
-                    'pass2 = '.$userPaswC. '<br/>'.
-                    'email = '.$userMail;   
-            echo '</p>';
+        global $userNotUniq;
+        global $userInserted;
+
+        if ($userNotUniq) {
+            echo 'Такой пользователь уже есть!<br/>';
+        } else {
+            echo 'Проверка уникальности прошла успешно!<br/>';
+        }
+        if ($userInserted) {
+            echo 'Ползователь добавлен в БД!<br/>ID = ' . $userInserted;
+        } else {
+            echo 'Ползователь <b>НЕ</b> добавлен в БД!<br/>';
+        }
         ?>
     </body>
 </html>
--- webtech.sql
+++ webtech.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
+Source Server         : local
 Source Server Version : 50535
 Source Host           : localhost:3306
 Source Database       : webtech
@@ -10,22 +10,40 @@ Target Server Type    : MYSQL
 Target Server Version : 50535
 File Encoding         : 65001
 
-Date: 2017-10-09 12:30:31
+Date: 2017-11-20 15:35:55
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `t_files`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_files`;
+CREATE TABLE `t_files` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `name_stored` varchar(256) NOT NULL,
+  `name_original` varchar(256) NOT NULL,
+  `size` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_files
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `t_users`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_users`;
 CREATE TABLE `t_users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `log` varchar(64) NOT NULL,
   `pass` varchar(64) NOT NULL,
   `email` varchar(64) NOT NULL,
   `name` varchar(64) DEFAULT NULL,
-  `regdt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `regdt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `quota` int(10) unsigned NOT NULL DEFAULT '10' COMMENT 'дисковая квота, Мб',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_t_user_log` (`log`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
@@ -33,8 +51,5 @@ CREATE TABLE `t_users` (
 -- ----------------------------
 -- Records of t_users
 -- ----------------------------
-INSERT INTO `t_users` VALUES ('3', 'wd', '3a760f3a7956c3f44bd23a79ca58ce4ac0af673b', 'a', 'da', '2017-10-06 14:15:35');
-INSERT INTO `t_users` VALUES ('4', 'qqwe', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'dfbhzdrh', 'arh', '2017-10-06 14:15:51');
-INSERT INTO `t_users` VALUES ('6', 'aaa', '7e240de74fb1ed08fa08d38063f6a6a91462a815', 'aaa', 'aaa', '2017-10-09 09:46:42');
-INSERT INTO `t_users` VALUES ('7', 'bbb', '5cb138284d431abd6a053a56625ec088bfb88912', 'aaa', 'aaa', '2017-10-09 10:06:09');
-INSERT INTO `t_users` VALUES ('8', 'ccc', 'f36b4825e5db2cf7dd2d2593b3f5c24c0311d8b2', 'ccc', 'ccc', '2017-10-09 10:13:12');
+INSERT INTO `t_users` VALUES ('0', 'aaa', '7e240de74fb1ed08fa08d38063f6a6a91462a815', 'aaa', 'aaa', '2017-10-09 09:46:42', '10');
+
